7832795e445290dff21716b81853710330167174
shill1729 <52792611+shill1729@users.noreply.github.com>
2022-04-17 13:17:46 -0400

    add ctmc_stationary

--- R/dtmc_stationary.R
+++ R/dtmc_stationary.R
@@ -17,3 +17,23 @@ dtmc_stationary <- function (P)
   pi <- qr.solve(A, b)
   return(pi)
 }
+
+
+#' Compute the stationary distribution of a CTMC given a transition-rate matrix
+#'
+#' @param Q the transition-rate matrix for the given CTMC
+#'
+#' @description {Uses the basic linear-algebra constrained method to solve for the limiting distribution, if it exists.}
+#' @return vector
+#' @export ctmc_steady_state
+ctmc_steady_state <- function (Q)
+{
+  n <- nrow(Q)
+  W <- t(Q)
+  ones <- rep(1, n)
+  A <- rbind(W, ones)
+  b <- rep(0, n+1)
+  b[n+1] <- 1
+  pi <- qr.solve(A, b)
+  return(pi)
+}
